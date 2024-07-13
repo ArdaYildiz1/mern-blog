a57ea15156001ac11a333c5f99819ae2242eca5f
ArdaYildiz1 <ardayildiz1074@gmail.com>
2024-07-13 14:15:17 +0300

    feat: add posts

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,18 +1,51 @@
+import {Navigate} from 'react-router-dom';
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
-export default function Create() {
+export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
+    const [redirect, setRedirect] = useState(false);
     
+    async function createNewPost(ev) {
+        ev.preventDefault();
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+        });
+        if (response.ok) {
+            setRedirect(true);
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={'/'} />;
+    }
     return(
-        <form>
-            <input type="title" placeholder={'Title'} />
-            <input type="summary" placeholder={'Summary'} />
-            <input type="file" />
-            <ReactQuill />
+        <form onSubmit={createNewPost}>
+            <input 
+            type="title" 
+            placeholder={'Title'}  
+            value={title} 
+            onChange={ev => setTitle(ev.target.value)}/>
+            <input 
+            type="summary" 
+            placeholder={'Summary'}
+            value={summary}
+            onChange={ev => setSummary(ev.target.value)} />
+            <input type="file"
+            onChange={ev => setFiles(ev.target.files)}/>
+            <ReactQuill 
+            value={content}
+            onChange={newValue => setContent(newValue)} />
             <button style={{marginTop:'10px'}}>Create post</button>
         </form>
     );
