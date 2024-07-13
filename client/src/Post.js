e5ebd9608a8945310fb0276eb7668f93b48c7d76
ArdaYildiz1 <ardayildiz1074@gmail.com>
2024-07-13 22:37:47 +0300

    feat: print posts

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,18 +1,18 @@
-export default function Post() {
+import {formatISO9075} from 'date-fns';
+
+export default function Post({title, summary, cover, content, createdAt}) {
     return (
         <div className="post">
             <div className="image">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Optimus_Prime_Model.JPG/330px-Optimus_Prime_Model.JPG"/>
             </div>
             <div className="texts">
-                <h2>Glorius Optimus Prime coming soon!</h2>
+                <h2>{title}</h2>
                 <p className="info">
                     <a className="author">Arda YILDIZ</a>
-                    <time>03.07.2024 12:30</time>
+                    <time>{formatISO9075(new Date(createdAt))}</time>
                 </p>
-                <p className="summary">"Glorius Optimus Prime" sounds like a majestic and powerful name, reminiscent of a heroic character or a significant entity. It could
-                    inspire visions of
-                    grandeur, leadership, or even technological superiority, much like the iconic Transformers character, Optimus Prime.</p>
+                <p className="summary">{summary}</p>
             </div>
         </div>
     );
